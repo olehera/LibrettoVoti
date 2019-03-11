6d5660dacb743e0cd890c209bb16094c31ab383c
olehera <47741249+olehera@users.noreply.github.com>
2019-03-11 13:52:48 +0100

    Metodi standard.

--- src/libretto/Voto.java
+++ src/libretto/Voto.java
@@ -8,6 +8,36 @@ public class Voto {
 	private String corso;
 	private LocalDate data;     //Calendar
 	
+	public Voto(int voto, String corso, LocalDate data) {
+		this.voto = voto;
+		this.corso = corso;
+		this.data = data;
+	}
+
+	public int getVoto() {
+		return voto;
+	}
+
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+
+	public String getCorso() {
+		return corso;
+	}
+
+	public void setCorso(String corso) {
+		this.corso = corso;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
 	
 	
 
